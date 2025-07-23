009e2a449407faa80143e3c3885ca03f99b7ee1c
Saurav Singh Mohanta <sauravsinghmohanta44@gmail.com>
2025-07-23 22:22:38 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,27 @@
-module "ec2_nginx" {
-  source            = "./ec2-nginx"
-  region            = "eu-north-1"
-  public_key_path   = "~/.ssh/id_rsa.pub"
-  private_key_path  = "~/.ssh/id_rsa"
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "eu-north-1"
+}
+
+variable "ami" {
+  description = "AMI ID for Ubuntu"
+  type        = string
+  default     = "ami-0901f13eb74a20662" # Ubuntu 20.04 for eu-north-1
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "public_key_path" {
+  description = "Path to public SSH key"
+  type        = string
+}
+
+variable "private_key_path" {
+  description = "Path to private SSH key"
+  type        = string
 }
