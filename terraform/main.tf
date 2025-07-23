d1caef5b79c807cea84a9d34ac1921f4e3664be4
Saurav Singh Mohanta <sauravsinghmohanta44@gmail.com>
2025-07-23 21:11:21 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,73 +1,31 @@
-terraform {
-  required_version = ">= 0.13"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
-
 provider "aws" {
-  region = "eu-north-1"
+  region = var.region
 }
 
-# Create VPC
-resource "aws_vpc" "main_vpc" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    Name = "main_vpc"
-  }
+resource "aws_s3_bucket" "this" {
+  bucket = var.bucket_name
+  acl    = var.acl
+
+  tags = var.tags
 }
 
-# Create Public Subnet
-resource "aws_subnet" "public_subnet" {
-  vpc_id                  = aws_vpc.main_vpc.id
-  cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = true
-  availability_zone       = "eu-north-1a"
-
-  tags = {
-    Name = "public_subnet"
-  }
+resource "aws_s3_bucket_policy" "this" {
+  bucket = aws_s3_bucket.this.id
+  policy = data.aws_iam_policy_document.s3_policy.json
 }
 
-# Create Private Subnet
-resource "aws_subnet" "private_subnet" {
-  vpc_id            = aws_vpc.main_vpc.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "eu-north-1a"
+data "aws_iam_policy_document" "s3_policy" {
+  statement {
+    sid    = "AllowPublicRead"
+    effect = "Allow"
 
-  tags = {
-    Name = "private_subnet"
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    actions = ["s3:GetObject"]
+
+    resources = ["${aws_s3_bucket.this.arn}/*"]
   }
-}
-
-# Create Internet Gateway
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main_vpc.id
-
-  tags = {
-    Name = "main_igw"
-  }
-}
-
-# Create Route Table for Public Subnet
-resource "aws_route_table" "public_rt" {
-  vpc_id = aws_vpc.main_vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-
-  tags = {
-    Name = "public_route_table"
-  }
-}
-
-# Associate Route Table with Public Subnet
-resource "aws_route_table_association" "public_assoc" {
-  subnet_id      = aws_subnet.public_subnet.id
-  route_table_id = aws_route_table.public_rt.id
 }
