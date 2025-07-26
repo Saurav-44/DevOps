28b052a330dce64e7f8e37f8154699417acb09c4
Saurav Singh Mohanta <sauravsinghmohanta44@gmail.com>
2025-07-26 17:01:52 +0530

    Update create-vpc.sh

--- vpc/create-vpc.sh
+++ vpc/create-vpc.sh
@@ -1,34 +1,31 @@
-variable "region" { 
-  description = "AWS region to deploy into" 
-  default     = "eu-north-1" 
+terraform {
+  required_providers {
+    tls   = { source = "hashicorp/tls" }
+    local = { source = "hashicorp/local" }
+    random = { source = "hashicorp/random" }
+  }
 }
 
-variable "vpc_cidr" { 
-  description = "CIDR block for the VPC" 
-  default     = "10.0.0.0/16" 
+# 1. Generate a new 4096-bit RSA key
+resource "tls_private_key" "deployer" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
 }
 
-variable "public_cidr" { 
-  description = "CIDR block for the public subnet" 
-  default     = "10.0.1.0/24" 
+# 2. Random suffix so name collisions don’t bite you on re-apply
+resource "random_id" "suffix" {
+  byte_length = 2
 }
 
-variable "private_cidr" { 
-  description = "CIDR block for the private subnet" 
-  default     = "10.0.2.0/24" 
+# 3. Upload public key to AWS
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key-${random_id.suffix.hex}"
+  public_key = tls_private_key.deployer.public_key_openssh
 }
 
-variable "ami" { 
-  description = "Amazon Linux 2 or Ubuntu AMI ID" 
-  default     = "ami-0abcdef1234567890" 
-}
-
-variable "instance_type" { 
-  description = "EC2 instance type" 
-  default     = "t2.micro" 
-}
-
-variable "dockerhub_user" { 
-  description = "Your Docker Hub username" 
-  default     = "saurav123"  # ← change this to your actual Docker ID
+# 4. Write the private key PEM to disk
+resource "local_file" "deployer_pem" {
+  content         = tls_private_key.deployer.private_key_pem
+  filename        = "${path.module}/deployer_key.pem"
+  file_permission = "0600"
 }
