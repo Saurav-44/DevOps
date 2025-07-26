b3711182a6cd62b27743e436d864423dc61d0e27
Saurav Singh Mohanta <sauravsinghmohanta44@gmail.com>
2025-07-26 16:53:46 +0530

    Update create-vpc.sh

--- vpc/create-vpc.sh
+++ vpc/create-vpc.sh
@@ -1,7 +1,11 @@
-FROM node:18-alpine
-WORKDIR /app
-COPY package.json ./
-RUN npm install --production
-COPY index.js ./
-EXPOSE 3000
-CMD ["node", "index.js"]
+variable "region"        { default = "eu-north-1" }
+variable "vpc_cidr"      { default = "10.0.0.0/16" }
+variable "public_cidr"   { default = "10.0.1.0/24" }
+variable "private_cidr"  { default = "10.0.2.0/24" }
+variable "ami"           { default = "ami-0abcdef1234567890" }   # Update to a Linux AMI in your region
+variable "instance_type" { default = "t2.micro" }
+variable "key_name"      { description = "Your EC2 key pair name" }
+variable "private_key_path" {
+  description = "Path to your private key file (for SSH provisioners)"
+}
+variable "dockerhub_user" { description = "saurav123" }
