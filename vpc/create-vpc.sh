6a2e193a04083e9b2a64df0dd40c0df233b57238
Saurav Singh Mohanta <sauravsinghmohanta44@gmail.com>
2025-07-26 17:00:10 +0530

    Update create-vpc.sh

--- vpc/create-vpc.sh
+++ vpc/create-vpc.sh
@@ -1,11 +1,34 @@
-variable "region"        { default = "eu-north-1" }
-variable "vpc_cidr"      { default = "10.0.0.0/16" }
-variable "public_cidr"   { default = "10.0.1.0/24" }
-variable "private_cidr"  { default = "10.0.2.0/24" }
-variable "ami"           { default = "ami-0abcdef1234567890" }   # Update to a Linux AMI in your region
-variable "instance_type" { default = "t2.micro" }
-variable "key_name"      { description = "Your EC2 key pair name" }
-variable "private_key_path" {
-  description = "Path to your private key file (for SSH provisioners)"
+variable "region" { 
+  description = "AWS region to deploy into" 
+  default     = "eu-north-1" 
 }
-variable "dockerhub_user" { description = "saurav123" }
+
+variable "vpc_cidr" { 
+  description = "CIDR block for the VPC" 
+  default     = "10.0.0.0/16" 
+}
+
+variable "public_cidr" { 
+  description = "CIDR block for the public subnet" 
+  default     = "10.0.1.0/24" 
+}
+
+variable "private_cidr" { 
+  description = "CIDR block for the private subnet" 
+  default     = "10.0.2.0/24" 
+}
+
+variable "ami" { 
+  description = "Amazon Linux 2 or Ubuntu AMI ID" 
+  default     = "ami-0abcdef1234567890" 
+}
+
+variable "instance_type" { 
+  description = "EC2 instance type" 
+  default     = "t2.micro" 
+}
+
+variable "dockerhub_user" { 
+  description = "Your Docker Hub username" 
+  default     = "saurav123"  # ← change this to your actual Docker ID
+}
