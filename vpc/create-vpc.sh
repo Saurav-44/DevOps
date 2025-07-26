4f59f9350fa4f742850db6903e45ecd03c5b282d
Saurav Singh Mohanta <sauravsinghmohanta44@gmail.com>
2025-07-26 16:42:25 +0530

    Update create-vpc.sh

--- vpc/create-vpc.sh
+++ vpc/create-vpc.sh
@@ -1,15 +1,7 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-# from app/scripts:
-cd ../backend
-docker build -t saurav123/node-mysql-backend:latest .
-docker push saurav123/node-mysql-backend:latest
-
-cd ../frontend
-docker build -t saurav123/simple-frontend:latest .
-docker push saurav123/simple-frontend:latest
-
-# back to project root
-cd ../../
-
+FROM node:18-alpine
+WORKDIR /app
+COPY package.json ./
+RUN npm install --production
+COPY index.js ./
+EXPOSE 3000
+CMD ["node", "index.js"]
